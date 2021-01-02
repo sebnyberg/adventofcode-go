d7ee2618fd7dffb29a59c52f84e8ff81414b2a04
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2021-01-02 21:49:30 +0100

    done with day 3

--- aoc2015/a15d3/a_test.go
+++ aoc2015/a15d3/a_test.go
@@ -2,10 +2,10 @@ package a15d3
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"testing"
 
+	"github.com/sebnyberg/adventofcode/must"
 	"github.com/stretchr/testify/require"
 )
 
@@ -15,7 +15,7 @@ func Test_day(t *testing.T) {
 	for range deliveries {
 		houses++
 	}
-	require.Equal(t, 0, houses)
+	require.Equal(t, 2631, houses)
 }
 
 type Point struct {
@@ -25,26 +25,28 @@ type Point struct {
 
 func deliverPresents() map[Point]int {
 	f := must.Open("input")
-	// f := strings.NewReader("^>v<")
 	sc := bufio.NewScanner(f)
 	sc.Split(bufio.ScanRunes)
-	pos := Point{0, 0}
-	deliveries := map[Point]int{pos: 1}
-	for sc.Scan() {
-		switch ch := sc.Text(); {
-		case ch == ">":
-			pos.X++
-		case ch == "<":
-			pos.X--
-		case ch == "v":
-			pos.Y--
-		case ch == "^":
-			pos.Y++
-		default:
-			log.Fatalf("bad character %v\n", string(ch))
-		}
-		fmt.Printf("+%v\n", deliveries)
-		deliveries[pos]++
+	santaPos := []Point{{0, 0}, {0, 0}}
+	deliveries := map[Point]int{{0, 0}: 2}
+	for i := 0; sc.Scan(); i++ {
+		move(&santaPos[i%2], sc.Text())
+		deliveries[santaPos[i%2]]++
 	}
 	return deliveries
+}
+
+func move(pos *Point, ch string) {
+	switch ch {
+	case ">":
+		pos.X++
+	case "<":
+		pos.X--
+	case "v":
+		pos.Y--
+	case "^":
+		pos.Y++
+	default:
+		log.Fatalf("bad character %v\n", string(ch))
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/sebnyberg/adventofcode
 
 go 1.14
 
-require github.com/stretchr/testify v1.6.1
+require (
+	github.com/stretchr/testify v1.6.1
+	golang.org/x/tools/gopls v0.5.1 // indirect
+)
